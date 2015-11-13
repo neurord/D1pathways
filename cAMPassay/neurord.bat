ee8a00ac67109a467bb90af6e095b96a4dcb4a00
Avrama Blackwell <avrama@gmu.edu>
2015-11-13 14:06:44 -0500

    Keeping the lower Abuff injection, slower GasGTP hydrolysis didn't work Increased PDE10 from 100 to 110 to lower basal and peak cAMP (sims running) Still need method to reduce cAMP peak while prolonging cAMP elevation

---
+++ cAMPassay/neurord.bat
@@ -0,0 +1,4 @@
+java -jar /home/neuroware/stochdif/stochdiff-3.0.0-8.jar Modelv3dt_cAMPassayGapGiPKAphosD1R-PD4E100-Da0.1.xml &
+java -jar /home/neuroware/stochdif/stochdiff-3.0.0-8.jar Modelv3dt_cAMPassayGapGiPKAphosD1R-PD4E100-Da10.xml&
+java -jar /home/neuroware/stochdif/stochdiff-3.0.0-8.jar Modelv3dt_cAMPassayGapGiPKAphosD1R-PD4E100-Da0.3.xml& 
+java -jar /home/neuroware/stochdif/stochdiff-3.0.0-8.jar Modelv3dt_cAMPassayGapGiPKAphosD1R-PD4E100-Da1.xml&
