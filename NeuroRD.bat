6d783ba6edb40a043dea15674c6b43f80d0ca5b0
Avrama Blackwell <avrama@gmu.edu>
2015-12-03 10:28:26 -0500

    Updated full model with parameters from cAMPassay Full model response to Da bath, Ca bath, and blocked are qualitatively correct Problem still remains with insufficient increase in Dp34 with PP2B block Began simulations of dendrite with spine

--- NeuroRD.bat
+++ NeuroRD.bat
@@ -2,9 +2,13 @@
 #To start multiple NeuroRD simulations at once, add & at end of each line.
 #run the batch file using: at -f NeuroRD.bat NOW (or you can specify a different time)
 set -ex
-java -jar /home/neuroware/stochdif/v2.1.9/stochdiff2.1.9.jar Model_MSPNmerge.xml output/Model_MSPNmerge.out > output/MSPNmerge.log
+java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9um-Block1.xml output/Model_MSPNmerge0.9um-Block1 &
+java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9um-Block2.xml output/Model_MSPNmerge0.9um-Block2 &
+java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9umPKAcD1R_CaB.xml output/Model_MSPNmerge0.9umPKAcD1R_CaB &
+java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9umPKAcD1R_DaB.xml output/Model_MSPNmerge0.9umPKAcD1R_DaB &
+#java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmergeSpine.xml output/Model_MSPNmergeSpine &
 
 #alternative:
 #To run things in parallel (instead of batch file):
-parallel --gnu java -Dstochdiff.writers=text -jar /home/neuroware/stochdif/stochdiff-3.0.0-1.jar ::: Model_cAMPassaySoma*.xml
+#parallel --gnu java -Dstochdiff.writers=text -jar /home/neuroware/stochdif/stochdiff-3.0.0-8.jar ::: Model_cAMPassaySoma*.xml
 
