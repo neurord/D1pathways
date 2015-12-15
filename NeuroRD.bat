313994a355fd61bc1d172c78805d5b165069d2ca
Avrama Blackwell <avrama@gmu.edu>
2015-12-15 18:29:00 -0500

    Good parameters for beginning simulations with stimulation to spine 1. IC files for spine: IC_MSPNmergeSpinePKAcD1R_PDEhigh.xml have been adjusted for ~ss based on 200 sec non-stim sim Note that it is dangerous to have PKAc or PKAr non-zero, as they will be unbalanced.  Starting stim at 20 sec might be OK. 2. Rxn file for spine: Rxn_MSPNmergePKAcD1R.xml has the higher PLC catalytic rates Note that PIP2 is regenerated from Ip3, but Dag converts to PA.  So, we could potentially get more Dag than initial PIP2.  Need to think about reactions to fix this. 3. Possibly spine head values of receptors and enzymes are too low compared to membrane values 4. The morphology file for spine sim has dendrite called dend.  If you use a morphology file with dendrite called dendrite, the simulation won't run.  I.e., the region names in the IC file must match the morphology file 5. I have included an updated version of python analysis files. 6. These Model (please use: Model_MSPNmergeSpine-PDEhigh.xml) and Reaction files are adjusted to stochdif (neuroRD) version 3.  This version is 3-10x faster than the old neuroRD.  Please get the new version from: https://github.com/neurord/stochdiff/releases/tag/v3.0.0-rc1, download: stochdiff-3.0.0-all-deps.jar 7. Issues still remaining: CyA inhibition of PP2B isn't doing as much as experiments; Da bath is increasing Dp34PP1 too much 8. For some reason, the latest notebook file isn't included in this commit.  Git isn't seeing it.

--- NeuroRD.bat
+++ NeuroRD.bat
@@ -2,12 +2,23 @@
 #To start multiple NeuroRD simulations at once, add & at end of each line.
 #run the batch file using: at -f NeuroRD.bat NOW (or you can specify a different time)
 set -ex
-java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9um-Block1.xml output/Model_MSPNmerge0.9um-Block1 > /dev/null&
-java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9um-Block2.xml output/Model_MSPNmerge0.9um-Block2 > /dev/null&
-java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9umPKAcD1R_CaB.xml output/Model_MSPNmerge0.9umPKAcD1R_CaB > /dev/null&
-java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9umPKAcD1R_DaB.xml output/Model_MSPNmerge0.9umPKAcD1R_DaB > /dev/null&
-java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9umPKAcD1R_DaCaB.xml output/Model_MSPNmerge0.9umPKAcD1R_DaCaB > /dev/null&
-java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmergeSpine.xml output/Model_MSPNmergeSpine > /dev/null&
+#java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9um-PDEhigh-BlockOA.xml output/Model_MSPNmerge0.9um-PDEhigh-BlockOA > /dev/null&
+#java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9um-PDEhigh-BlockCyA.xml output/Model_MSPNmerge0.9um-PDEhigh-BlockCyA > /dev/null&
+#java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9umPKAcD1R-PDEhigh-BathCa.xml output/Model_MSPNmerge0.9umPKAcD1R-PDEhigh-BathCa > /dev/null&
+#java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9umPKAcD1R-PDEhigh-BathDa.xml output/Model_MSPNmerge0.9umPKAcD1-PDEhigh-BathDa > /dev/null&
+#java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9umPKAcD1R-PDEhigh-BathDaCa.xml output/Model_MSPNmerge0.9umPKAcD1R-PDEhigh-BathDaCa > /dev/null&
+java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmergeSpine-PDEhigh.xml output/Model_MSPNmergeSpine-PDEhigh > /dev/null&
+java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9um-PDEhigh-BlockCyA-BathDa.xml output/Model_MSPNmerge0.9um-PDEhigh-BlockCyA-BathDa > /dev/null&  
+java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9um-PDEhigh-BlockCyA-BathDaCa.xml output/Model_MSPNmerge0.9um-PDEhigh-BlockCyA-BathDaCa > /dev/null&
+
+#java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9um-PDElow-BlockOA.xml output/Model_MSPNmerge0.9um-PDElow-BlockOA > /dev/null&
+#java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9um-PDElow-BlockCyA.xml output/Model_MSPNmerge0.9um-PDElow-BlockCyA > /dev/null&
+#java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9umPKAcD1R-PDElow-BathCa.xml output/Model_MSPNmerge0.9umPKAcD1R-PDElow-BathCa > /dev/null&
+#java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9umPKAcD1R-PDElow-BathDa.xml output/Model_MSPNmerge0.9umPKAcD1-PDElow-BathDa > /dev/null&
+#java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9umPKAcD1R-PDElow-BathDaCa.xml output/Model_MSPNmerge0.9umPKAcD1R-PDElow-BathDaCa > /dev/null&
+#java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmergeSpine-PDElow.xml output/Model_MSPNmergeSpine-PDElow > /dev/null&
+java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9um-PDElow-BlockCyA-BathDa.xml output/Model_MSPNmerge0.9um-PDElow-BlockCyA-BathDa > /dev/null&
+java -jar -Dstochdiff.log=false /home/neuroware/stochdif/stochdiff-3.0.0-9.jar Model_MSPNmerge0.9um-PDElow-BlockCyA-BathDaCa.xml output/Model_MSPNmerge0.9um-PDElow-BlockCyA-BathDaCa > /dev/null&
 
 #alternative:
 #To run things in parallel (instead of batch file):
--- plot_utils.py
+++ plot_utils.py
@@ -1,1 +1,71 @@
-/home/avrama/python/NeuroRDanal/plot_utils.py
+import numpy as np
+from matplotlib import pyplot
+
+def plot_setup(plot_molecules,param_list,param_name):
+     pyplot.ion()
+     fig,axes=pyplot.subplots(len(plot_molecules), 1,sharex=True) #n rows,  1 column
+     col_inc=[0.5,0.5]
+     scale=['lin','lin']
+     numpar=[0,0]
+     for i,paramset in enumerate(param_list):
+          numpar=len(paramset)
+          if numpar>1:
+               col_inc[i]=1.0/(len(paramset))
+               print "plot_setup: col_inc,scale=",col_inc, scale
+     return fig,axes,col_inc,scale,numpar
+
+def plottrace(plotmol,time,plotarray,parval,axes,fig,colinc,scale,parlist):
+     print "plottrace: parval,parlist:", parval, parlist
+     if len(parlist)==0:
+          p0=p1=0
+     else:
+          if np.shape(parlist[1])[0]==0:
+               p0=p1=parlist[0].index(parval)
+          elif np.shape(parlist[0])[0]==0:
+               p0=p1=parlist[1].index(parval)
+          else:
+               p0=parlist[0].index(parval[0])
+               p1=parlist[1].index(parval[1])
+     for imol,mol in enumerate(plotmol):
+          if imol==0:
+               axes[imol].plot(time,plotarray[:,imol],label=parval,color=(p0*colinc[0],0,p1*colinc[1]))
+          else:
+               axes[imol].plot(time,plotarray[:,imol],color=(p0*colinc[0],0,p1*colinc[1]))
+          axes[imol].set_ylabel(mol)#+' (nM)')
+          axes[imol].legend(fontsize=8, loc='best')
+     axes[imol].set_xlabel('Time (sec)')
+     fig.canvas.draw()
+     return
+
+def plotss(plot_mol,xparval,ss):
+    fig,axes=pyplot.subplots(len(plot_mol), 1,sharex=True)
+    for imol,mol in enumerate(plot_mol):
+        axes[imol].plot(xparval,ss[:,imol],'o',label=mol)
+        axes[imol].set_ylabel('nM')
+        if max(xparval)/min(xparval)>100:
+            axes[imol].set_xscale("log")
+        axes[imol].legend(fontsize=8, loc='best')
+    fig.canvas.draw()
+    return
+
+#The files are sorted on the parameters, assumes no hyphens in root filename
+def file_tuple(fnames,params):
+     ftuple=[]
+     par0list=[]
+     par1list=[]
+     for fname in fnames:
+            parts=fname.split('-')
+            parval0=parts[1].split(params[0])[1]
+            if (parval0 not in par0list):
+                 par0list.append(parval0)
+            print 'pu: fname, par0:',fname,par0list
+            if len(params)>1:
+                    parval1=parts[2].split(params[1])[1]
+                    ftuple.append((fname,(parval0,parval1)))
+                    if (parval1 not in par1list):
+                         par1list.append(parval1)
+                    print 'pu: par1:',par1list
+            else:
+                    ftuple.append((fname,parval0))
+     return ftuple,[par0list,par1list]
+
